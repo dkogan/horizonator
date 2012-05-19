8b3acfb86c71b9f4a2c5f55b6381c693c2cbb85d
Dima Kogan <dima@secretsauce.net>
2012-05-18 22:02:51 -0700

    initial commit of the start of the fit utility

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 LDLIBS += -lGL -lGLEW -lglut
 CFLAGS = -std=gnu99 -g -O3
 
-readhorizon: readhorizon.o
+all: readhorizon
 
 CFLAGS += -I/usr/include/opencv2
 LDLIBS += -lopencv_imgproc -lopencv_highgui -lopencv_core
 
 clean:
-	rm -rf readhorizon *.o
+	rm -rf fit readhorizon *.o
 
 .PHONY: clean
 
---
+++ fit.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <opencv2/core/core_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/highgui/highgui_c.h>
+
+#define IRON_ANGLE      10.8373931367 /* angle of view for my iron mt photo */
+
+int main(int argc, char* argv[])
+{
+  if( argc != 2 )
+  {
+    fprintf(stderr, "Must have input filename as the only argument\n");
+    return 1;
+  }
+
+  IplImage* src = cvLoadImage( argv[1], CV_LOAD_IMAGE_COLOR);
+  assert(src);
+
+  CvSize size = cvGetSize(src);
+
+  IplImage* remapped = cvCreateImage(size, 8, 3);
+  assert(remapped);
+
+  CvMat* mapx = cvCreateMat(size.height, size.width, CV_32FC1);
+  assert(mapx); assert(mapx->step == size.width*sizeof(float));
+  CvMat* mapy = cvCreateMat(size.height, size.width, CV_32FC1);
+  assert(mapy); assert(mapy->step == size.width*sizeof(float));
+
+  for(int j=0; j<size.height; j++ )
+  {
+    for( int i=0; i<size.width; i++ )
+    {
+      mapy->data.fl[i + mapy->cols*j] = j;
+
+      float angle = 180.0f / M_PI *
+        atanf( ( (float)i - (float)(mapx->cols-1)/2.0f )/ (24.0f /35.0f * (float)mapx->cols ) );
+      mapx->data.fl[i + mapx->cols*j] =
+        (angle / IRON_ANGLE + 0.5f) * (float)(mapx->cols-1);
+    }
+  }
+
+  cvRemap(src, remapped,
+          mapx, mapy,
+          CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS,
+          cvScalarAll(0) );
+
+  assert( cvSaveImage("remapped.png", remapped, 0) );
+
+  cvReleaseImage( &src );
+  cvReleaseImage( &remapped );
+  cvReleaseMat( &mapx );
+  cvReleaseMat( &mapy );
+
+
+  return 0;
+}
