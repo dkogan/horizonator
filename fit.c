271813337bb32f077b4d95767704369cb79661d4
Dima Kogan <dima@secretsauce.net>
2012-05-31 23:55:29 -0700

    fit program updated. Edges are now extracted

--- fit.c
+++ fit.c
@@ -8,19 +8,126 @@
 
 #define IRON_ANGLE 72.2 /* angle of view for my iron mt photo */
 
+static void extractEdges( const IplImage* img, CvMat* edges, int presmooth )
+{
+  CvSize size = cvGetSize(img);
+
+  CvMat* img_channels[3];
+  img_channels[0] = cvCreateMat( size.height, size.width, CV_8UC1 );
+  img_channels[1] = cvCreateMat( size.height, size.width, CV_8UC1 );
+  img_channels[2] = cvCreateMat( size.height, size.width, CV_8UC1 );
+
+  CvMat* img_channel_edges[3];
+  img_channel_edges[0] = cvCreateMat( size.height, size.width, CV_16SC1 );
+  img_channel_edges[1] = cvCreateMat( size.height, size.width, CV_16SC1 );
+  img_channel_edges[2] = cvCreateMat( size.height, size.width, CV_16SC1 );
+
+  cvSplit(img,
+          img_channels[0],
+          img_channels[1],
+          img_channels[2],
+          NULL);
+
+  if( presmooth )
+  {
+    cvSmooth(img_channels[0], img_channels[0], CV_GAUSSIAN, presmooth, presmooth, 0.0, 0.0);
+    cvSmooth(img_channels[1], img_channels[2], CV_GAUSSIAN, presmooth, presmooth, 0.0, 0.0);
+    cvSmooth(img_channels[2], img_channels[2], CV_GAUSSIAN, presmooth, presmooth, 0.0, 0.0);
+  }
+
+  cvLaplace(img_channels[0], img_channel_edges[0], 3);
+  cvLaplace(img_channels[1], img_channel_edges[1], 3);
+  cvLaplace(img_channels[2], img_channel_edges[2], 3);
+
+  cvAbs( img_channel_edges[0], img_channel_edges[0] );
+  cvAbs( img_channel_edges[1], img_channel_edges[1] );
+  cvAbs( img_channel_edges[2], img_channel_edges[2] );
+
+  cvZero(edges);
+  cvAdd(edges, img_channel_edges[0], edges, NULL);
+  cvAdd(edges, img_channel_edges[1], edges, NULL);
+  cvAdd(edges, img_channel_edges[2], edges, NULL);
+
+  cvReleaseMat( &img_channels[0] );
+  cvReleaseMat( &img_channels[1] );
+  cvReleaseMat( &img_channels[2] );
+
+  cvReleaseMat( &img_channel_edges[0] );
+  cvReleaseMat( &img_channel_edges[1] );
+  cvReleaseMat( &img_channel_edges[2] );
+}
+
+static CvPoint alignImages( const CvMat* img, const CvMat* pano )
+{
+  CvSize img_size  = cvGetSize(img);
+  CvSize pano_size = cvGetSize(pano);
+
+  CvMat* img_float = cvCreateMat( img_size.height, img_size.width, CV_32FC1);
+  assert( img_float );
+
+  CvMat* pano_float = cvCreateMat( pano_size.height, pano_size.width, CV_32FC1);
+  assert( pano_float );
+
+  CvMat* match = cvCreateMat( pano_size.height - img_size.height + 1,
+                              pano_size.width  - img_size.width  + 1,
+                              CV_32FC1);
+
+  cvConvert( img,  img_float );
+  cvConvert( pano, pano_float );
+  cvMatchTemplate( pano_float, img_float, match, CV_TM_SQDIFF );
+
+  double minval, maxval;
+  CvPoint minpoint, maxpoint;
+  cvMinMaxLoc( match, &minval, &maxval, &minpoint, &maxpoint, NULL );
+
+  cvReleaseMat( &match );
+  cvReleaseMat( &img_float );
+  cvReleaseMat( &pano_float );
+
+  return maxpoint;
+}
+
 int main(int argc, char* argv[])
 {
-  if( argc != 2 )
+  if( argc != 3 )
   {
-    fprintf(stderr, "Must have input filename as the only argument\n");
+    fprintf(stderr, "Usage: %s panorender.png photo.jpg\n", argv[0]);
     return 1;
   }
 
-  IplImage* src = cvLoadImage( argv[1], CV_LOAD_IMAGE_COLOR);
-  assert(src);
+  IplImage* pano = cvLoadImage( argv[1], CV_LOAD_IMAGE_COLOR);
+  assert(pano);
 
-  CvSize size = cvGetSize(src);
+  IplImage* img  = cvLoadImage( argv[2], CV_LOAD_IMAGE_COLOR);
+  assert(img);
 
+  CvSize pano_size = cvGetSize(pano);
+  CvMat* pano_edges = cvCreateMat( pano_size.height, pano_size.width, CV_16SC1 );
+  extractEdges(pano, pano_edges, 0);
+
+  CvSize img_size = cvGetSize(img);
+  CvMat* img_edges = cvCreateMat( img_size.height, img_size.width, CV_16SC1 );
+  extractEdges(img, img_edges, 9);
+  cvSmooth(img_edges, img_edges, CV_GAUSSIAN, 13, 13, 0.0, 0.0);
+
+  // cvSmooth(img_channels[0], img_channels[0], CV_GAUSSIAN, presmooth, presmooth, 0.0, 0.0);
+
+  CvPoint offset = alignImages( img_edges, pano_edges );
+  printf("offset: x,y: %d %d\n", offset.x, offset.y );
+
+
+
+  cvReleaseMat  ( &pano_edges );
+  cvReleaseMat  ( &img_edges );
+  cvReleaseImage( &pano );
+  cvReleaseImage( &img );
+
+  return 0;
+}
+
+#if 0
+static void undistort(void)
+{
   IplImage* remapped = cvCreateImage(size, 8, 3);
   assert(remapped);
 
@@ -51,14 +158,11 @@ int main(int argc, char* argv[])
           mapx, mapy,
           CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS,
           cvScalarAll(0) );
-
-  assert( cvSaveImage("remapped.png", remapped, 0) );
-
-  cvReleaseImage( &src );
   cvReleaseImage( &remapped );
   cvReleaseMat( &mapx );
   cvReleaseMat( &mapy );
 
-
-  return 0;
 }
+
+#endif
+
