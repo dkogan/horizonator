615673522f5e1a6a39424ea16851a9f528846e31
Dima Kogan <dima@secretsauce.net>
2013-04-25 23:49:15 -0700

    getting elevation in meters, not feet

--- parse_features.pl
+++ parse_features.pl
@@ -14,7 +14,7 @@ my @key = split '\|', $key_line;
 my ($col_name) = grep { $key[$_] eq 'FEATURE_NAME'  } 0..$#key;
 my ($col_lat ) = grep { $key[$_] eq 'PRIM_LAT_DEC'  } 0..$#key;
 my ($col_lon ) = grep { $key[$_] eq 'PRIM_LONG_DEC' } 0..$#key;
-my ($col_ele ) = grep { $key[$_] eq 'ELEV_IN_FT'    } 0..$#key;
+my ($col_ele ) = grep { $key[$_] eq 'ELEV_IN_M'     } 0..$#key;
 
 while(<>)
 {
