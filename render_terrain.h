22dc81ccf6e5e6d8737741c913f3b379d3df719d
Dima Kogan <dima@secretsauce.net>
2013-04-29 01:43:35 -0700

    renderer can save render to a file; backend can do BGR to support this

--- render_terrain.h
+++ render_terrain.h
@@ -3,6 +3,6 @@
 #include <stdbool.h>
 #include <opencv2/core/core_c.h>
 
-IplImage* render_terrain( float view_lat, float view_lon, float* elevation );
+IplImage* render_terrain( float view_lat, float view_lon, float* elevation, bool do_bgr );
 bool render_terrain_to_window( float view_lat, float view_lon );
 
