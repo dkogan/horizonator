943c77212b79c9696eb57ea93c1d710f13a37a2d
Dima Kogan <dima@secretsauce.net>
2013-06-09 20:50:41 -0700

    render_terrain.h now works from c++

--- render_terrain.h
+++ render_terrain.h
@@ -3,6 +3,15 @@
 #include <stdbool.h>
 #include <opencv2/core/core_c.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 IplImage* render_terrain( float view_lat, float view_lon, float* elevation, bool do_bgr );
 bool render_terrain_to_window( float view_lat, float view_lon );
 
+
+#ifdef __cplusplus
+}
+#endif
