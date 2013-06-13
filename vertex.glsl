4efea602bd2c82709a0272913e7cce14eb3cf6b1
Dima Kogan <dima@secretsauce.net>
2013-06-13 14:32:56 -0700

    I no longer have _header.glsl. Those didn't need to be separate

--- vertex.glsl
+++ vertex.glsl
@@ -1,6 +1,20 @@
 /* -*- c -*- */
 
-/* Makefile includes vertex_header.glsl here */
+#version 110
+
+uniform float view_z;
+uniform float renderStartN, renderStartE;
+uniform float DEG_PER_CELL;
+uniform float view_lon;
+uniform float view_lat;
+uniform float sin_view_lat, cos_view_lat;
+
+uniform float aspect;
+varying float red;
+
+const float Rearth = 6371000.0;
+const float pi     = 3.14159265358979;
+const float znear  = 100.0, zfar = 30000.0;
 
 
 void main(void)
