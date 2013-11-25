1c155d45fa025e661c1e7c3b6e14c053b034a222
Dima Kogan <dima@secretsauce.net>
2013-11-24 22:38:22 -0800

    comment

--- render_terrain.h
+++ render_terrain.h
@@ -8,6 +8,8 @@ extern "C"
 {
 #endif
 
+// returns an image. The caller is responsible for freeing the memory with
+// cvReleaseImage
 IplImage* render_terrain( float view_lat, float view_lon, float* elevation, bool do_bgr );
 bool render_terrain_to_window( float view_lat, float view_lon );
 
