25c30409f3d3f2e81bf04e98887b6c58cbe3733c
Dima Kogan <dima@secretsauce.net>
2014-05-10 17:15:19 -0700

    added some nifty and simple benchmarking code

--- render_terrain.c
+++ render_terrain.c
@@ -50,6 +50,33 @@ static int offscreen_w = OFFSCREEN_W_DEFAULT;
 static int offscreen_h = OFFSCREEN_H_DEFAULT;
 
 
+static inline uint64_t rdtscll()
+{
+    // taken from the wikipedia rdtsc article
+    uint32_t lo, hi;
+    /* We cannot use "=A", since this would use %rax on x86_64 */
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return (uint64_t)hi << 32 | lo;
+}
+// to time a function call "f(a,b,c)", change it to "BENCH(f,a,b,c)". This uses
+// __auto_type, so gcc-4.9 or higher is required
+#define BENCH( name, args... )                                          \
+    ({                                                                  \
+        uint64_t t0 = rdtscll();                                        \
+        __auto_type res = name(args);                                   \
+        uint64_t t1 = rdtscll();                                        \
+        fprintf(stderr, #name " took %g cycles\n", (double)(t1-t0));    \
+        res;                                                            \
+    })
+#define BENCH_VOID( name, args... )                                     \
+    ({                                                                  \
+        uint64_t t0 = rdtscll();                                        \
+        name(args);                                                     \
+        uint64_t t1 = rdtscll();                                        \
+        fprintf(stderr, #name " took %g cycles\n", (double)(t1-t0));    \
+        1;                                                              \
+    })
+
 
 static bool loadGeometry( float view_lat, float view_lon,
                           float* elevation_out )
