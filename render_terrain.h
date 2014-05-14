56f11562f5232744bd81b8d9ce21d01767f6a282
Dima Kogan <dima@secretsauce.net>
2014-05-14 04:04:10 -0700

    I now only draw labels for unoccluded peaks

--- render_terrain.h
+++ render_terrain.h
@@ -14,11 +14,14 @@ IplImage* render_terrain( float view_lat, float view_lon, float* elevation,
                           int width, float fovy_deg, // render parameters. negative to take defaults
                           bool do_bgr );
 bool render_terrain_to_window( float view_lat, float view_lon );
+
 bool render_pick(// output
                  float* lon,  float* lat,
 
                  // input
                  int x, int y );
+
+const CvMat* render_terrain_getdepth(void);
 
 #ifdef __cplusplus
 }
